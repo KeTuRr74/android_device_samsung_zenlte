0135f702aea2a3380354dbc1895600e598422eb6
Bunyamin OZDEMIR <avatarfightgames@gmail.com>
2017-12-25 20:43:35 +0100

    Initial Release

---
+++ zenltexx.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := device/samsung/zenltexx
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.baseband.rc
+
+# Radio
+# cpboot-daemon for modem
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# call the proprietary setup
+$(call inherit-product, vendor/samsung/zenltexx/zenltexx-vendor.mk)
