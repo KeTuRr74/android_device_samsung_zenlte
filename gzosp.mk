51021abbe6a35a03a980d90b7754136e15745402
Bunyamin OZDEMIR <avatarfightgames@gmail.com>
2017-12-25 20:46:49 +0100

    Added gzosp.mk

---
+++ gzosp.mk
@@ -0,0 +1,22 @@
+# Inherit device configuration
+$(call inherit-product, device/samsung/zenltexx/zenltexx.mk)
+$(call inherit-product, device/samsung/zenlte-common/zenlte.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := gzosp_zenltexx
+PRODUCT_DEVICE := zenltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G928F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=zenltexx
